afebbd049af7a9b853c5e45c7be05b64abbec36b
InnovateBham_6 <InnovateBham_6@admins-iMac.local>
2017-08-02 17:03:44 -0500

    initial commit

---
+++ server.js
@@ -0,0 +1,64 @@
+var http = require('http');
+var url = require('url');
+var fs = require('fs');
+var path = require('path');
+var shortid = require('shortid');
+
+var jsonPath = path.join(__dirname, 'data.json');
+
+console.log('starting server');
+http.createServer(function(req, res) {
+    var parsedUrl = url.parse(req.url);
+
+    if (parsedUrl.pathname === '/chirps' && req.method === 'GET') {
+ 
+        fs.readFile(jsonPath, function(err, file) {
+            if (err) {
+                res.writeHead(500);
+                res.end('Could not read file');
+            }
+
+            res.write(file);
+            res.end();
+        });
+    } else if (parsedUrl.pathname === '/chirps' && req.method === 'POST') {
+        var chunks = '',
+            data;
+
+        req.on('data', function(chunk) {
+            chunks += chunk;
+
+            if (chunks.length > 1e6) {
+                req.connection.destroy();
+            }
+
+            data = JSON.parse(chunks);
+        });
+
+        fs.readFile(jsonPath, 'utf-8', function(err, file) {
+            if (err) {
+                res.writeHead(500);
+                res.end('Could not read file');
+            }
+
+            var arr = JSON.parse(file);
+
+            data.id = shortid.generate();
+
+            arr.push(data);
+
+            fs.writeFile(jsonPath, JSON.stringify(arr), function(err, success) {
+                if (err) {
+                    res.writeHead(500);
+                    res.end('Couldn\'t successfull store data');
+                } else {
+                    res.writeHead(201, 'Created');
+                    res.end(JSON.stringify(arr));
+                }
+            });
+        });
+    } else if (parsedUrl.pathname === '/chirps/one/' && req.method === 'DELETE') {
+        
+    }
+})
+.listen(3000);
